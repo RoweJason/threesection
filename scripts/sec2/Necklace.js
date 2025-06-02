f6babe93edc36246edf15dbcee04e3d27b63d666
RoweJason <luoshijie2003@qq.com>
2025-06-03 00:47:29 +0800

    feat：sec2结构完成

--- scripts/sec2/Necklace.js
+++ scripts/sec2/Necklace.js
@@ -9,14 +9,14 @@ const nktl = gsap.timeline({
         start: "top top",
         end: "bottom bottom",
         scrub: 1,
-        // pin: true,
+        pin: true,
         markers: true
     }
 });
 // 让项链在滚动到屏幕中间时锁定在中间
-nktl.to(nk, {
-    // y: () => (window.innerHeight / 2 - nk.offsetHeight / 2),
-    y : () => window.innerHeight + nk.offsetHeight / 2,
-    duration: 1,
-    ease: "none"
-}, 0);
+// nktl.to(nk, {
+//     // y: () => (window.innerHeight / 2 - nk.offsetHeight / 2),
+//     y : () => window.innerHeight + nk.offsetHeight / 2,
+//     duration: 1,
+//     ease: "none"
+// }, 0);
