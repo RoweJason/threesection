b31e77ef6b423a1190e9b66f28ee80aaf469a057
RoweJason <luoshijie2003@qq.com>
2025-06-06 15:57:13 +0800

    feat：项链固定

--- scripts/sec2/Necklace.js
+++ scripts/sec2/Necklace.js
@@ -1,22 +1,15 @@
 const nk = document.querySelector(".necklace");
+const sec2_2 = document.querySelector(".sec2-container-2");
+const sec2_outer = document.querySelector(".sec2-outer")
 gsap.registerPlugin(ScrollTrigger);
 
 // 滚动到屏幕中间时，锁定项链位于中间
 
-const nktl = gsap.timeline({
-    scrollTrigger: {
-        trigger: nk,
-        start: "top top",
-        end: "bottom bottom",
-        scrub: 1,
-        pin: true,
-        markers: true
-    }
-});
-// 让项链在滚动到屏幕中间时锁定在中间
-// nktl.to(nk, {
-//     // y: () => (window.innerHeight / 2 - nk.offsetHeight / 2),
-//     y : () => window.innerHeight + nk.offsetHeight / 2,
-//     duration: 1,
-//     ease: "none"
-// }, 0);
+ScrollTrigger.create({
+    trigger: sec2_outer,
+    scrub: true,
+    start: "1500 top",
+    end: "bottom bottom",
+    markers: true,
+    pin: nk
+})
--- scripts/sec2/Scrolling.js
+++ scripts/sec2/Scrolling.js
@@ -5,34 +5,6 @@ gsap.registerPlugin(ScrollTrigger);
 
 // 创建时间线，每一屏移动100%
 const maintl = gsap.timeline({
-    scrollTrigger: {
-        trigger: outer,
-        pin: true,
-        scrub: 1,
-        // snap: {
-        //     snapTo: (value, self) => {
-        //         const sections = secs.length - 1;
-        //         const snapIndex = value * sections;
-        //         // 超过一半吸附下一屏，否则保持当前位置
-        //         if (snapIndex % 1 > 0.5) {
-        //             return (Math.floor(snapIndex) + 1) / sections;
-        //         }
-        //         return value;
-
-        //     },
-        //     duration: { min: 0.1, max: 0.3 },
-        //     delay: 0,
-        //     ease: "power3.inOut"
-        // },
-        end: () => "+=" + (secs[0].offsetHeight * secs.length - window.innerHeight),
-    }
+    
 });
 
-// 每一屏依次向上移动
-for (let i = 1; i < secs.length; i++) {
-    maintl.to(secs, {
-        yPercent: -100 * i,
-        duration: 1,
-        ease: "none"
-    });
-}
