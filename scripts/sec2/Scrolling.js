cf6ccf9cde44e7fc44ed8e0ca0d721d260c13b04
RoweJason <luoshijie2003@qq.com>
2025-06-12 18:43:42 +0800

    style：换图，sec3彩虹流动

--- scripts/sec2/Scrolling.js
+++ scripts/sec2/Scrolling.js
@@ -3,7 +3,6 @@ const outer = document.querySelector('.sec2-outer');
 // 注册ScrollTrigger插件
 gsap.registerPlugin(ScrollTrigger);
 
-// 创建时间线，每一屏移动100%
 const maintl = gsap.timeline({
     
 });
---
+++ scripts/sec3/rainbowflow.js
@@ -0,0 +1,38 @@
+const sec3_container2 = document.querySelector(".sec3-container-2");
+const sec3_container3 = document.querySelector(".sec3-container-3");
+const rainbow1 = document.querySelector(".rainbow1");
+const rainbow2 = document.querySelector(".rainbow2");
+
+// 注册scroll插件
+gsap.registerPlugin(ScrollTrigger);
+
+// 随滚动进度改变css变量scroll_progress1的值(彩虹1进度)
+ScrollTrigger.create({
+    trigger: sec3_container2,
+    start: "50 540",
+    end: "800 bottom",
+    // markers: true,
+    onUpdate: (self) => {
+        rainbow1.style.setProperty('--scroll_progress1', self.progress);
+    },
+});
+// 随滚动进度改变css变量scroll_progress2的值(彩虹2进度)
+ScrollTrigger.create({
+    trigger: sec3_container3,
+    start: "top 900",
+    end: "1500 bottom",
+    markers: true,
+    onUpdate: (self) => {
+        rainbow2.style.setProperty('--scroll_progress2', self.progress);
+        console.log(self.progress);
+    },
+});
+// 随滚动进度改变山的位置
+ScrollTrigger.create({
+    trigger: sec3_container4,
+    start: "top 900",
+    end: "1500 bottom",
+    onUpdate: (self) => {
+        
+    },
+});
