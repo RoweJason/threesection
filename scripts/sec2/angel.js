af3f3647a025b6ddd537a16b00560becb7aeb73d
RoweJason <luoshijie2003@qq.com>
2025-06-15 22:32:12 +0800

    del：删除了没用到的图片

--- scripts/sec2/angel.js
+++ scripts/sec2/angel.js
@@ -1,9 +1,9 @@
-const nk = document.querySelector(".angel");
+const angel = document.querySelector(".angel");
 const sec2_2 = document.querySelector(".sec2-container-2");
 const sec2_outer = document.querySelector(".sec2-outer")
 gsap.registerPlugin(ScrollTrigger);
 
-// 滚动到屏幕中间时，锁定项链位于中间
+// 滚动到屏幕中间时，锁定天使位于中间
 
 ScrollTrigger.create({
     trigger: sec2_outer,
@@ -11,5 +11,5 @@ ScrollTrigger.create({
     start: "1300 top",
     end: "bottom bottom",
     markers: true,
-    pin: nk
+    pin: angel
 })
